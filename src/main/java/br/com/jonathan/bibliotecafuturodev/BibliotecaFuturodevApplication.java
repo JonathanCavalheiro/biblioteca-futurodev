7f946fa3177f7fc2704dcba7137a7b7b42692388
Jonathan Cavalheiro <jonathan.delima@hotmail.com>
2022-10-23 00:00:50 -0300

    initial commit

---
+++ src/main/java/br/com/jonathan/bibliotecafuturodev/BibliotecaFuturodevApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonathan.bibliotecafuturodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaFuturodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaFuturodevApplication.class, args);
+	}
+
+}
